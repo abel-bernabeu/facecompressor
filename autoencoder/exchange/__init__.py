228f1b6040e5091dbcc8f2a405a7d481aba0c784
Abel Bernabeu <abel.bernabeu@gmail.com>
2020-06-06 21:45:36 +0200

    Added a class encapsulating the bit stream writing
    As part of ticket #9 I looked at the options for writing grayscale
    PNG files with an arbitrary number of bits per pixel. However, the
    support for fancy formats like 1, 2, 4, 8 and 16 bits is only
    available for paletted images in PIL. Tried also OpenCV2 where the
    support was even worse, not even having the option of forcing the
    number of bits by using a palette.
    
    It then became evident to me that writting PNG files with arbitrary
    number of bits, even though it is supported by the PNG spec, is
    just not supported in the python libraries and going that way would
    be like openning a can of worms.
    
    As alternative I am going to write the quantized tensors as binary
    files with a format defined by myself. For this task I need a bit
    stream writer and a reader.
    
    After giving a try to the bitstream python package I found it was too
    complex and too broken for being useful, so I wrote my own bit stream
    writer out of need.
    
    Will also make my own reader on another patch for addressing #10 (the
    reader).
    
    When the tensor writer (#9) and the reader (#10), then I will document
    the used format (#12).

---
+++ autoencoder/exchange/__init__.py
@@ -0,0 +1,49 @@
+class BitStreamWriter:
+
+    def __init__(self):
+        self.bits = []
+
+    def put(self, value, num_bits=1):
+        mask = 1
+        for index in range(num_bits):
+            is_set = (value & mask) != 0
+            self.bits.append(is_set)
+            mask <<= 1
+
+    def pad(self, num_bits=8):
+        remainder_bits = len(self.bits) % num_bits
+        if (remainder_bits > 0):
+            padding_bits = num_bits - remainder_bits
+            self.put(0, padding_bits)
+
+    def write(self, filename):
+        with open(filename, 'wb') as file:
+            self.pad(8)
+            num_bytes = len(self.bits) // int(8)
+            data = bytearray(num_bytes)
+            for byte_index in range(num_bytes):
+                value = 0
+                for bit_index in range(8):
+                    if (self.bits[byte_index * 8 + bit_index]):
+                        value |= (1 << bit_index)
+                data[byte_index] = value
+            file.write(data)
+            self.bits = []
+
+
+def test_bit_stream_writer():
+    from testfixtures import TempDirectory
+    import os
+    with TempDirectory() as d:
+        d.create()
+        filename = "tmp.txt"
+        os.chdir(d.getpath("") )
+        stream = BitStreamWriter()
+        stream.put(1,4)
+        stream.put(4,4)
+        stream.put(66,8)
+        stream.put(67,8)
+        stream.write(filename)
+        with open(filename, 'r') as file:
+            data = file.read()
+            assert(data == "ABC")
