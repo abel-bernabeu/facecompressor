a8904d7b2b4c6dd4f439c36df9a588723a1d9568
Abel Bernabeu <abel.bernabeu@gmail.com>
2020-07-08 00:24:36 +0200

    Removed a reference to the image package

--- autoencoder/__init__.py
+++ autoencoder/__init__.py
@@ -1,13 +1,11 @@
 import torch
 import sys
-import autoencoder.image
 
 def train():
     if (not torch.cuda.is_available()):
         print('Error: CUDA is needed for training')
         sys.exit(0)
     print ('TODO: training')
-    autoencoder.image.save()
 
 def encode():
     print ('TODO: encoding')
