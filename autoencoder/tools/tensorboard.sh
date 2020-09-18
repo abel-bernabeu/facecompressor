653badaf1e3e0b9810d331c8bf6c4cb3dcaba709
Abel Bernabeu <abel.bernabeu@gmail.com>
2020-09-18 14:46:10 +0000

    Added tensorboard command lines for experiments 6 and 7

--- autoencoder/tools/tensorboard.sh
+++ autoencoder/tools/tensorboard.sh
@@ -8,3 +8,5 @@ tensorboard --logdir share/experiment2/runs --port 6200 &
 tensorboard --logdir share/experiment3/runs --port 6300 &
 tensorboard --logdir share/experiment4/runs --port 6400 &
 tensorboard --logdir share/experiment5/runs --port 6500 &
+tensorboard --logdir share/experiment6/runs --port 6600 &
+tensorboard --logdir share/experiment7/runs --port 6700 &
